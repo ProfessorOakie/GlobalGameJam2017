a80e99b7d3d5313dbd613527e88c37c156490470
MxRComputer <drewoken@comcast.net>
2017-01-21 11:56:56 -0800

    Restrict CameraRig Y axis movement so you don't fly way high - Awu

---
+++ Assets/RestirctCameraYMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestirctCameraYMovement : MonoBehaviour {
+    GameObject cameraRig;
+    public int minY = 0;
+    public int maxY = 1;
+	// Use this for initialization
+	void Start () {
+        cameraRig = GameObject.FindWithTag("camera");   //Find the game object in the scene whose tag is "Ball".
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // If Player's Y exceeds minY or maxY..
+        if (cameraRig.transform.position.y <= minY || cameraRig.transform.position.y >= maxY)
+        {
+            // Create values between this range (minY to maxY) and store in yPos
+            float yPos = Mathf.Clamp(cameraRig.transform.position.y, minY, maxY);
+
+            // Assigns these values to the Transform.position component of the Player
+            cameraRig.transform.position = new Vector3(cameraRig.transform.position.x, yPos, 0);
+        }
+    }
+}
