3535ca92b848f128cacce768b94b5060f2a6d75f
Drew Okenfuss <okenfuss@usc.edu>
2017-01-22 03:36:59 -0800

    player heartbeat

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,72 @@ using UnityEngine;
 
 public class PlayerController  : MonoBehaviour {
 
+    private Rigidbody rig;
+
+    [SerializeField]
+    private float HeartbeatThreshold = 1.0f;
+    public float HeartbeatVolume = 1.0f;
+    private float HeartbeatVolumeInitial;
+    public float HeartbeatVolumeMax = 3.0f;
+    [SerializeField]
+    private AudioClip[] heartbeatSounds;
+    private AudioSource source;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+        source = GetComponent<AudioSource>();
+        HeartbeatVolumeInitial = HeartbeatVolume;
+        StartCoroutine(Heartbeat());
+    }
+
+    void CheckCollidingWithMonster(Collider collider)
+    {
+        if (collider.gameObject.GetComponent<Monster>())
+        {
+            // collided with monster
+            // TODO: play dying animations
+            Die();
+        }
+
+    }
+
+    private void Die()
+    {
+        Debug.Log("Player Has Died");
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        if(rig.velocity.magnitude < HeartbeatThreshold)
+        {
+            IntensifyHeartbeat(Time.deltaTime);
+        }
+    }
+
+    //Not moving
+    private void IntensifyHeartbeat(float timestep)
+    {
+        HeartbeatVolume += timestep * 0.3f;//how fast volume increases
+        if (HeartbeatVolume > HeartbeatVolumeMax)
+            HeartbeatVolume = HeartbeatVolumeMax;
+    }
+    private void ResetHeartbeat()
+    {
+        HeartbeatVolume = HeartbeatVolumeInitial;
+    }
+
+    IEnumerator Heartbeat()
+    {
+        AudioClip clip = heartbeatSounds[Random.Range(0, heartbeatSounds.Length)];
+        source.PlayOneShot(clip, HeartbeatVolume);
+        //how long you wait between beats, scaled with volume
+        yield return new WaitForSeconds(clip.length + (HeartbeatVolumeMax - HeartbeatVolume)/HeartbeatVolumeMax * 2.0f);
+        StartCoroutine(Heartbeat());
+    }
+    
+
     private void OnCollisionEnter(Collision collision)
     {
         CheckCollidingWithMonster(collision.collider);
@@ -22,23 +88,6 @@ public class PlayerController  : MonoBehaviour {
     private void OnTriggerStay(Collider other)
     {
         CheckCollidingWithMonster(other);
-    }
-
-    void CheckCollidingWithMonster(Collider collider)
-    {
-        if (collider.gameObject.GetComponent<Monster>())
-        {
-            // collided with monster
-            // TODO: play dying animations
-            Die();
-        }
-
-    }
-
-    private void Die()
-    {
-        Debug.Log("Player Has Died");
-        Destroy(gameObject);
     }
 
 }
