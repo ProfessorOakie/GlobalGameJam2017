7af2f3c646271d095363edac8b955a47930d91e0
Anshul Pendse <anshulpendse@gmail.com>
2017-01-22 03:31:19 -0800

    monster sonar work

--- Assets/Scripts/MonsterSound.cs
+++ Assets/Scripts/MonsterSound.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace MonsterSoundSystem
-{
-    public class MonsterSound : MonoBehaviour
+public class MonsterSound : MonoBehaviour
     {
         public AudioClip[] monsterIdle;
         public AudioClip[] monsterCharge;
@@ -14,30 +12,33 @@ namespace MonsterSoundSystem
         // call these methods when you want to play the sound 
         // using MonsterSoundSystem;
         // MonsterSound.IdleSound();
-        public void Shatter()
+        public float Shatter()
         {
             audio = GetComponent<AudioSource>();
             audio.PlayOneShot(shatter, 0.7f);
+            return shatter.length;
         } 
-        public void IdleSound()
+        public float IdleSound()
         {
             audio = GetComponent<AudioSource>();
             int random = Random.Range(0, monsterIdle.Length);
             audio.PlayOneShot(monsterIdle[random], 0.7f);
+            return monsterIdle[random].length;
         }
 
-        public void ChargeSound()
+        public float ChargeSound()
         {
             audio = GetComponent<AudioSource>();
             int random = Random.Range(0, monsterCharge.Length);
             audio.PlayOneShot(monsterCharge[random], 0.9f);
+            return monsterCharge[random].length;
         }
 
-        public void PulseSound()
+        public float PulseSound()
         {
             audio = GetComponent<AudioSource>();
             int random = Random.Range(0, monsterPulse.Length);
             audio.PlayOneShot(monsterPulse[random], 0.6f);
+            return monsterPulse[random].length;
         }
-    }
 }
